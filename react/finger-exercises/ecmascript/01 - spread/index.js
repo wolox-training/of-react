a1dee9b105973fe1e1316b8c4b12d9aabcc92eea
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-05-20 13:48:53 -0300

    Passing first reverseMerge test.

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -17,6 +17,13 @@ export function copy(param) {
   return Object.assign(param.constructor(), param);
 }
 
-export function reverseMerge() {
-
+export function reverseMerge(arr1, arr2) {
+  const arrResult = [];
+  for (var i = 0; i < arr2.length; i++) {
+    arrResult.push(arr2[i]);
+  }
+  for (var j = 0; j < arr1.length; j++) {
+    arrResult.push(arr1[j]);
+  }
+  return arrResult;
 }
