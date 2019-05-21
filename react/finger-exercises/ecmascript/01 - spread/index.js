9aa2e6d8164016f02b00c873a87dbfbd02e1289c
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-05-21 09:20:37 -0300

    Refactor code based on review.

--- react/finger-exercises/ecmascript/01 - spread/index.js
+++ react/finger-exercises/ecmascript/01 - spread/index.js
@@ -1,28 +1,17 @@
 import { isArray } from './utils';
 
 export function min(...args) {
-  if (args.length > 0) {
-    if (args.length > 1) {
-      return Math.min(...args);
-    }
-    if (isArray(args[0])) {
-      return Math.min(...args[0]);
-    }
+  if (args.length) {
+    if (args.length > 1) return Math.min(...args);
+    if (isArray(args[0])) return Math.min(...args[0]);
     return Math.min(args[0]);
   }
   return undefined;
 }
 
-export function copy(param) {
-  return Object.assign(param.constructor(), param);
-}
+export const copy = param => Object.assign(param.constructor(), param);
 
-export function reverseMerge(arr1, arr2) {
-  const arrResult = [];
-  arrResult.push(...arr2);
-  arrResult.push(...arr1);
-  return arrResult;
-}
+export const reverseMerge = (arr1, arr2) => [...arr2, ...arr1];
 
 export function filterAttribs(arg) {
   const copyArg = copy(arg);
