3b79cb3e98eaa62a323c2c4e339d204f29a28cf5
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-05-23 11:52:02 -0300

    Making Square component interactive.

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-const Square = (props) => (
-  <button type="button" className={styles.square}>
-    {props.value}
-  </button>
-);
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button 
+      type="button" 
+      className={styles.square} 
+      onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
 
 export default Square;
