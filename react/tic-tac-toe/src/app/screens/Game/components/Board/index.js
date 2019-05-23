7ba08770c13716eeb2ff95bff8959236bad3f4ba
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-05-23 13:19:44 -0300

    Lifting state up with problems.

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -5,8 +5,26 @@ import Square from '../Square';
 import styles from './styles.module.scss';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState = ({squares:squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -2,23 +2,15 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button 
-      type="button" 
-      className={styles.square} 
-      onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
+const Square = (props) => (
+  <button 
+    className={styles.square} 
+    onClick={() => {props.onClick()}}
+    type="button" 
+  >
+    {props.value}
+  </button>
+);
+
 
 export default Square;
