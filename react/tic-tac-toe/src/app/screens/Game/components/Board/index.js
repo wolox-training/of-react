9886dd394ad7c8497163d0268afd5a4a9c6fd0ea
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-05-24 10:29:39 -0300

    Passing Board to functional component from class component.

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -1,42 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Square from '../Square';
 
 import styles from './styles.module.scss';
 
-class Board extends Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    return (
+const Board = (props) => 
+    (
       <div>
         <div className={styles.boardRow}>
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          <Square
+          value={props.squares[0]}
+          onClick={() => props.onClick(0)}
+          />
+          <Square
+          value={props.squares[1]}
+          onClick={() => props.onClick()}
+          />
+          <Square
+          value={props.squares[2]}
+          onClick={() => props.onClick(2)}
+          />
         </div>
         <div className={styles.boardRow}>
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          <Square
+          value={props.squares[3]}
+          onClick={() => props.onClick(3)}
+          />
+          <Square
+          value={props.squares[4]}
+          onClick={() => props.onClick(4)}
+          />
+          <Square
+          value={props.squares[5]}
+          onClick={() => props.onClick(5)}
+          />
         </div>
         <div className={styles.boardRow}>
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          <Square
+          value={props.squares[6]}
+          onClick={() => props.onClick(6)}
+          />
+          <Square
+          value={props.squares[7]}
+          onClick={() => props.onClick(7)}
+          />
+          <Square
+          value={props.squares[8]}
+          onClick={() => props.onClick(8)}
+          />
         </div>
       </div>
-    );
-  }
-}
-
-
+  );
 
 export default Board;
