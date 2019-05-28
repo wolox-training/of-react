de296c33d8ffb427c3b0200f093b9defdd9926b2
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-05-28 10:01:32 -0300

    Fix bad identation.

--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -19,7 +19,7 @@ class Game extends Component {
   }
 
   handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
