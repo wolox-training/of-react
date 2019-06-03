7883384cb7042730b9a0669c76958c18cab00de9
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-06-03 12:10:55 -0300

    Saving error message in the store.

--- react/tic-tac-toe/src/redux/table-matches/reducer.js
+++ react/tic-tac-toe/src/redux/table-matches/reducer.js
@@ -3,6 +3,7 @@ import { actions } from './actions';
 
 const initialState = {
   matches: [],
+  errorMessage: ''
 };
 
 function reducer(state = initialState, action) {
@@ -10,9 +11,9 @@ function reducer(state = initialState, action) {
     case actions.GET_MATCHES:
       return state;
     case actions.GET_MATCHES_SUCCESS:
-      return {matches : [...action.payload]}
+      return {...state, matches : [...action.payload]}
     case actions.GET_MATCHES_FAILURE:
-      return state;
+      return {...state, errorMessage: action.payload};
     default:
       return state;
   }
