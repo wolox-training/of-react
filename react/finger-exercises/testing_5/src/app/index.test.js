9333b6561dc281167433f9fdffbecef053984edf
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-06-18 14:32:49 -0300

    Pass firsts tests.

--- react/finger-exercises/testing_5/src/app/index.test.js
+++ react/finger-exercises/testing_5/src/app/index.test.js
@@ -6,8 +6,24 @@ describe('H1 Text', () => {
   it(
     'h1 loads correctly',
     async () => {
-      const browser = await Puppeteer.launch();
+      const browser = await Puppeteer.launch({ args: ['--no-sandbox'] });
       const page = await browser.newPage();
+
+      page.emulate({
+        viewport: {
+          width: 500,
+          height: 2400
+        },
+        userAgent: ''
+      });
+
+      await page.goto(TEST_BASE_URL);
+      await page.waitForSelector('.app-title');
+
+      const html = await page.$eval('.app-title', e => e.innerHTML);
+      expect(html).toBe('Welcome to React');
+
+      browser.close();
     },
     16000
   );
--- react/finger-exercises/testing_5/src/app/screens/Dashboard/components/SignupForm/index.test.js
+++ react/finger-exercises/testing_5/src/app/screens/Dashboard/components/SignupForm/index.test.js
@@ -3,7 +3,7 @@ import Puppeteer from 'puppeteer';
 
 import { TEST_BASE_URL } from '../../../../../config/test';
 
-// import { FIELDS } from './constants';
+import { FIELDS } from './constants';
 
 const person = {
   firstName: Faker.name.firstName(),
@@ -15,21 +15,93 @@ describe('Signup Form (use FIELDS constants)', () => {
   let browser;
   let page;
 
-  beforeAll(async () => {}); // start the browser
+  beforeAll(async () => {
+    browser = await Puppeteer.launch({ args: ['--no-sandbox'] });
+  }); // start the browser
 
-  beforeEach(async () => {}); // go to root page
+  beforeEach(async () => {
+    page = await browser.newPage();
+  }); // go to root page
 
-  afterAll(() => {}); // close browesr
+  afterAll(() => {
+    browser.close();
+  }); // close browesr
 
-  it('Can submit signup form', async () => {});
+  it('Can submit signup form', async () => {
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
 
-  it('Successful submit', async () => {}, 16000);
+    await page.goto(TEST_BASE_URL);
+    await page.$eval('.signup-form', form => form.submit());
+  });
 
-  it('3 fields are required', async () => {}, 16000);
+  it(
+    'Successful submit',
+    async () => {
+      page.emulate({
+        viewport: {
+          width: 500,
+          height: 900
+        },
+        userAgent: ''
+      });
 
-  it('First name is required', async () => {}, 16000);
+      await page.goto(TEST_BASE_URL);
+      await page.waitForSelector('.signup-form');
 
-  it('Last name is required', async () => {}, 16000);
+      await page.click(`input[name=${FIELDS[0].name}]`);
+      await page.type(`input[name=${FIELDS[0].name}]`, person.firstName);
 
-  it('Email is required', async () => {}, 16000);
+      await page.click(`input[name=${FIELDS[1].name}]`);
+      await page.type(`input[name=${FIELDS[1].name}]`, person.lastName);
+
+      await page.click(`input[name=${FIELDS[2].name}]`);
+      await page.type(`input[name=${FIELDS[2].name}]`, person.email);
+
+      await page.click('button[type=submit]');
+
+      const html = await page.$eval('.result', e => e.innerHTML);
+      expect(html).toBe('User has been created successfully');
+    },
+    16000
+  );
+
+  it(
+    '3 fields are required',
+    async () => {
+      page.emulate({
+        viewport: {
+          width: 500,
+          height: 900
+        },
+        userAgent: ''
+      });
+
+      await page.goto(TEST_BASE_URL);
+      await page.waitForSelector('.signup-form');
+
+      await page.click('button[type=submit]');
+
+      const html = await page.$eval(`.error-${FIELDS[0].name}`, e => e.innerHTML);
+      expect(html).toBe('Required');
+
+      const html1 = await page.$eval(`.error-${FIELDS[1].name}`, e => e.innerHTML);
+      expect(html1).toBe('Required');
+
+      const html2 = await page.$eval(`.error-${FIELDS[2].name}`, e => e.innerHTML);
+      expect(html2).toBe('Required');
+    },
+    16000
+  );
+
+  xit('First name is required', async () => {}, 16000);
+
+  xit('Last name is required', async () => {}, 16000);
+
+  xit('Email is required', async () => {}, 16000);
 });
