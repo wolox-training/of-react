88ea767f2a309bbbd9c4eb19311c46b7a1b9865e
Olivia Fernandez <olivia.fernandez@wolox.com.ar>
2019-06-19 09:18:10 -0300

    Finishing puppeteer tests and change test.js in config to constantest.js because it has no tests in it.

--- react/finger-exercises/testing_5/src/app/index.test.js
+++ react/finger-exercises/testing_5/src/app/index.test.js
@@ -1,30 +1,26 @@
 import Puppeteer from 'puppeteer';
 
-import { TEST_BASE_URL } from '../config/test';
+import { TEST_BASE_URL } from '../config/constantest';
 
 describe('H1 Text', () => {
-  it(
-    'h1 loads correctly',
-    async () => {
-      const browser = await Puppeteer.launch({ args: ['--no-sandbox'] });
-      const page = await browser.newPage();
+  it('h1 loads correctly', async () => {
+    const browser = await Puppeteer.launch({ args: ['--no-sandbox'] });
+    const page = await browser.newPage();
 
-      page.emulate({
-        viewport: {
-          width: 500,
-          height: 2400
-        },
-        userAgent: ''
-      });
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400
+      },
+      userAgent: ''
+    });
 
-      await page.goto(TEST_BASE_URL);
-      await page.waitForSelector('.app-title');
+    await page.goto(TEST_BASE_URL);
+    await page.waitForSelector('.app-title');
 
-      const html = await page.$eval('.app-title', e => e.innerHTML);
-      expect(html).toBe('Welcome to React');
+    const html = await page.$eval('.app-title', e => e.innerHTML);
+    expect(html).toBe('Welcome to React');
 
-      browser.close();
-    },
-    16000
-  );
+    browser.close();
+  }, 16000);
 });
--- react/finger-exercises/testing_5/src/app/screens/Dashboard/components/SignupForm/index.test.js
+++ react/finger-exercises/testing_5/src/app/screens/Dashboard/components/SignupForm/index.test.js
@@ -1,7 +1,7 @@
 import Faker from 'faker';
 import Puppeteer from 'puppeteer';
 
-import { TEST_BASE_URL } from '../../../../../config/test';
+import { TEST_BASE_URL } from '../../../../../config/constantest';
 
 import { FIELDS } from './constants';
 
@@ -40,68 +40,108 @@ describe('Signup Form (use FIELDS constants)', () => {
     await page.$eval('.signup-form', form => form.submit());
   });
 
-  it(
-    'Successful submit',
-    async () => {
-      page.emulate({
-        viewport: {
-          width: 500,
-          height: 900
-        },
-        userAgent: ''
-      });
+  it('Successful submit', async () => {
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
 
-      await page.goto(TEST_BASE_URL);
-      await page.waitForSelector('.signup-form');
+    await page.goto(TEST_BASE_URL);
+    await page.waitForSelector('.signup-form');
 
-      await page.click(`input[name=${FIELDS[0].name}]`);
-      await page.type(`input[name=${FIELDS[0].name}]`, person.firstName);
+    await page.click(`input[name=${FIELDS[0].name}]`);
+    await page.type(`input[name=${FIELDS[0].name}]`, person.firstName);
 
-      await page.click(`input[name=${FIELDS[1].name}]`);
-      await page.type(`input[name=${FIELDS[1].name}]`, person.lastName);
+    await page.click(`input[name=${FIELDS[1].name}]`);
+    await page.type(`input[name=${FIELDS[1].name}]`, person.lastName);
 
-      await page.click(`input[name=${FIELDS[2].name}]`);
-      await page.type(`input[name=${FIELDS[2].name}]`, person.email);
+    await page.click(`input[name=${FIELDS[2].name}]`);
+    await page.type(`input[name=${FIELDS[2].name}]`, person.email);
 
-      await page.click('button[type=submit]');
+    await page.click('button[type=submit]');
 
-      const html = await page.$eval('.result', e => e.innerHTML);
-      expect(html).toBe('User has been created successfully');
-    },
-    16000
-  );
+    const html = await page.$eval('.result', e => e.innerHTML);
+    expect(html).toBe('User has been created successfully');
+  }, 16000);
 
-  it(
-    '3 fields are required',
-    async () => {
-      page.emulate({
-        viewport: {
-          width: 500,
-          height: 900
-        },
-        userAgent: ''
-      });
+  it('3 fields are required', async () => {
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
 
-      await page.goto(TEST_BASE_URL);
-      await page.waitForSelector('.signup-form');
+    await page.goto(TEST_BASE_URL);
+    await page.waitForSelector('.signup-form');
 
-      await page.click('button[type=submit]');
+    await page.click('button[type=submit]');
 
-      const html = await page.$eval(`.error-${FIELDS[0].name}`, e => e.innerHTML);
-      expect(html).toBe('Required');
+    const html = await page.$eval(`.error-${FIELDS[0].name}`, e => e.innerHTML);
+    expect(html).toBe('Required');
 
-      const html1 = await page.$eval(`.error-${FIELDS[1].name}`, e => e.innerHTML);
-      expect(html1).toBe('Required');
+    const html1 = await page.$eval(`.error-${FIELDS[1].name}`, e => e.innerHTML);
+    expect(html1).toBe('Required');
 
-      const html2 = await page.$eval(`.error-${FIELDS[2].name}`, e => e.innerHTML);
-      expect(html2).toBe('Required');
-    },
-    16000
-  );
+    const html2 = await page.$eval(`.error-${FIELDS[2].name}`, e => e.innerHTML);
+    expect(html2).toBe('Required');
+  }, 16000);
 
-  xit('First name is required', async () => {}, 16000);
+  it('First name is required', async () => {
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
 
-  xit('Last name is required', async () => {}, 16000);
+    await page.goto(TEST_BASE_URL);
+    await page.waitForSelector('.signup-form');
 
-  xit('Email is required', async () => {}, 16000);
+    await page.click('button[type=submit]');
+
+    const html = await page.$eval(`.error-${FIELDS[0].name}`, e => e.innerHTML);
+    expect(html).toBe('Required');
+  }, 16000);
+
+  it('Last name is required', async () => {
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
+
+    await page.goto(TEST_BASE_URL);
+    await page.waitForSelector('.signup-form');
+
+    await page.click('button[type=submit]');
+
+    const html = await page.$eval(`.error-${FIELDS[1].name}`, e => e.innerHTML);
+    expect(html).toBe('Required');
+  }, 16000);
+
+  it('Email is required', async () => {
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 900
+      },
+      userAgent: ''
+    });
+
+    await page.goto(TEST_BASE_URL);
+    await page.waitForSelector('.signup-form');
+
+    await page.click('button[type=submit]');
+
+    const html = await page.$eval(`.error-${FIELDS[2].name}`, e => e.innerHTML);
+    expect(html).toBe('Required');
+  }, 16000);
 });
